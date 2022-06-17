6f175a0e0e11f33155a234c6512824d26e9ca0d0
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-06-17 22:43:21 +0100

    ajout d'un message d'erreur si mauvais format

--- src/views/NewBillUI.js
+++ src/views/NewBillUI.js
@@ -56,6 +56,7 @@ export default () => {
 					<div class="col-half">
 						<label for="file" class="bold-label">Justificatif</label>
 						<input required type="file" class="form-control blue-border" data-testid="file" accept=".png, .jpg, .jpeg" />
+						<div class="error--image-entension">Votre justificatif doit être une image (JPG, JPEG ou PNG)</div>
 					</div>
 					</div>
 				</div>
