2f8516a6898c268fddd00242c330bec87844b17b
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-06-18 20:43:03 +0100

    Issue 3/6 Fixed : handleChangeFile

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -8,42 +8,53 @@ export default class NewBill {
 		this.store = store
 		const formNewBill = this.document.querySelector('form[data-testid="form-new-bill"]')
 		formNewBill.addEventListener('submit', this.handleSubmit)
-		const file = this.document.querySelector('input[data-testid="file"]')
-		file.addEventListener('change', this.handleChangeFile)
+		this.fileInput = this.document.querySelector('input[data-testid="file"]')
+		this.fileInput.addEventListener('change', this.handleChangeFile)
 		this.fileUrl = null
 		this.fileName = null
-		this.billId = null
+		this.fileExtension = false
 		new Logout({ document, localStorage, onNavigate })
 	}
-	handleChangeFile = e => {
-		e.preventDefault()
-		const file = this.document.querySelector('input[data-testid="file"]').files[0]
-		const filePath = e.target.value.split(/\\/g)
-		const fileName = filePath[filePath.length-1]
-		const formData = new FormData()
-		const email = JSON.parse(localStorage.getItem('user')).email
-		formData.append('file', file)
-		formData.append('email', email)
 
-		this.store
-			.bills()
-			.create({
-				data: formData,
-				headers: {
-					noContentType: true
-				}
-			})
-			.then(({fileUrl, key}) => {
-				console.log(fileUrl)
-				this.billId = key
-				this.fileUrl = fileUrl
-				this.fileName = fileName
-			}).catch(error => console.error(error))
+	handleChangeFile = e => {
+		this.formData = new FormData()
+		this.file = this.fileInput.files[0]
+		this.fileExtension = this.file.type.replace(/(.*)\//g, '')
+		const email = JSON.parse(localStorage.getItem('user')).email
+
+		if (this.fileExtension) {
+			this.document.querySelector('.error--image-entension').style.display = 'none'
+			this.fileUrl = e.target.value
+			this.fileName = this.file.name
+			this.formData.append('file', this.file)
+			this.formData.append('email', email)
+			this.store
+				.bills()
+				.create({
+					data: this.formData,
+					headers: {
+						noContentType: true
+					}
+				})
+				.then(({fileUrl, key}) => {
+					// eslint-disable-next-line no-unused-vars
+					fileUrl = this.fileUrl
+					this.billId = key
+				})
+				.catch(error => console.error(error))
+		}
+		else {
+			this.document.querySelector('.error--image-entension').style.display = 'block'
+			this.fileInput.value = null
+			console.error('Les extensions différentes de jpg, jpeg ou png ne sont pas autorisées')
+		}
 	}
+
 	handleSubmit = e => {
 		e.preventDefault()
 		console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector('input[data-testid="datepicker"]').value)
 		const email = JSON.parse(localStorage.getItem('user')).email
+
 		const bill = {
 			email,
 			type: e.target.querySelector('select[data-testid="expense-type"]').value,
@@ -62,6 +73,7 @@ export default class NewBill {
 	}
 
 	// not need to cover this function by tests
+	/* istanbul ignore next */
 	updateBill = (bill) => {
 		if (this.store) {
 			this.store
