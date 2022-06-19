4c3174547c64459d28835bfe4c36377e68c30ad0
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-06-19 01:25:54 +0100

    Issue 4/6 Fixed : stopImmediatePropagation() on handleEditTicket

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -84,25 +84,26 @@ export default class {
 	}
 
 	handleEditTicket(e, bill, bills) {
+		e.stopImmediatePropagation()
 		if (this.counter === undefined || this.id !== bill.id) this.counter = 0
 		if (this.id === undefined || this.id !== bill.id) this.id = bill.id
 		if (this.counter % 2 === 0) {
-			bills.forEach(b => {
-				$(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
+			console.log('bill cliqué')
+			bills.forEach(bill => {
+				$(`#open-bill${bill.id}`).css({ background: '#0D5AE5' }) //non clické
 			})
-			$(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
+			$(`#open-bill${bill.id}`).css({ background: '#2A2B35' }) // clické
 			$('.dashboard-right-container div').html(DashboardFormUI(bill))
 			$('.vertical-navbar').css({ height: '150vh' })
 			this.counter ++
 		} else {
 			$(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
-
-			$('.dashboard-right-container div').html(`
-        <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
-      `)
+			console.log('bill déclické')
+			$('.dashboard-right-container div').html(`<div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>`)
 			$('.vertical-navbar').css({ height: '120vh' })
 			this.counter ++
 		}
+
 		$('#icon-eye-d').click(this.handleClickIconEye)
 		$('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
 		$('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
@@ -131,18 +132,17 @@ export default class {
 	handleShowTickets(e, bills, index) {
 		if (this.counter === undefined || this.index !== index) this.counter = 0
 		if (this.index === undefined || this.index !== index) this.index = index
-		if (this.counter % 2 === 0) {
+		if (this.counter % 2 === 0) {  // Si on ouvre un onglet
 			$(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
 			$(`#status-bills-container${this.index}`)
 				.html(cards(filteredBills(bills, getStatus(this.index))))
 			this.counter ++
-		} else {
+		} else { // si on le ferme
 			$(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
 			$(`#status-bills-container${this.index}`)
 				.html('')
 			this.counter ++
 		}
-
 		bills.forEach(bill => {
 			$(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
 		})
