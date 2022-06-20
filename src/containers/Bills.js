b8aaff9b4e10f6803f48c755b6981165184a890c
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-06-21 00:47:41 +0100

    Fix : affiche seulement les bills valides

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -8,11 +8,15 @@ export default class {
 		this.onNavigate = onNavigate
 		this.store = store
 		const buttonNewBill = document.querySelector('button[data-testid="btn-new-bill"]')
-		if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
 		const iconEye = document.querySelectorAll('div[data-testid="icon-eye"]')
-		if (iconEye) iconEye.forEach(icon => {
-			icon.addEventListener('click', () => this.handleClickIconEye(icon))
-		})
+		try {
+			buttonNewBill.addEventListener('click', this.handleClickNewBill)
+			iconEye.forEach(icon => {
+				icon.addEventListener('click', () => this.handleClickIconEye(icon))
+			})
+		} catch (error) {
+			console.error(error)			
+		}
 		new Logout({ document, localStorage, onNavigate })
 	}
 
@@ -26,7 +30,8 @@ export default class {
 		$('#modaleFile').find('.modal-body').html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
 		$('#modaleFile').modal('show')
 	}
-
+	// not need to cover this function by tests
+	/* istanbul ignore next */
 	getBills = () => {
 		if (this.store) {
 			return this.store
@@ -40,19 +45,18 @@ export default class {
 								date: formatDate(doc.date),
 								status: formatStatus(doc.status)
 							}
-						} catch(e) {
+						} catch(error) {
 							// if for some reason, corrupted data was introduced, we manage here failing formatDate function
 							// log the error and return unformatted date in that case
-							console.log(e,'for',doc)
+							console.log(error, 'for', doc)
 							return {
 								...doc,
 								date: doc.date,
 								status: formatStatus(doc.status)
 							}
 						}
-					})
-					// console.log('length', bills.length)
-					// console.log({bills})
+					}).filter((bill) => bill.vat !== null)
+					console.log('bills :>> ', bills)
 					return bills
 				})
 		}
