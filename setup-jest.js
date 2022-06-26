8083bb94311ffba17d256ce62c2ad51685fa3fb7
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-06-26 21:37:39 +0200

    maj setup

--- setup-jest.js
+++ setup-jest.js
@@ -1,2 +1,10 @@
-import $ from 'jquery';
-global.$ = global.jQuery = $;
+/* eslint-disable no-undef */
+import $ from 'jquery'
+global.$ = global.jQuery = $
+
+import {localStorageMock} from './src/__mocks__/localStorage'
+
+export function setLocalStorage (user) {
+	Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+	window.localStorage.setItem('user', JSON.stringify({ type: user, email: 'test@email.com' }))
+}
